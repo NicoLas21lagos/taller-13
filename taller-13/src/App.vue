bf71503ef9b378f76a6dd11dc03f02c74a7f419f
Nicolas Llancaqueo <n.llancaqueo01@ufromail.cl>
2024-04-23 14:55:00 -0400

    Public branch
    Publicar rama

---
+++ taller-13/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <ReqList/>
+  </div>
+</template>
+
+<script>
+import ReqList from './components/ReqList.vue';
+
+export default {
+  components: {
+    ReqList
+  }
+};
+</script>
---
+++ taller-13/src/components/ReqList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <h1>Lista de Tareas</h1>
+      <input v-model="newTask" @keyup.enter="addTask" placeholder="Agregar nueva tarea">
+      <ul>
+        <li v-for="(task, index) in tasks" :key="index">
+          <span>{{ task }}</span>
+          <button @click="deleteTask(index)">Eliminar</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newTask: '',
+        tasks: []
+      };
+    },
+    methods: {
+      addTask() {
+        if (this.newTask.trim() !== '') {
+          this.tasks.push(this.newTask.trim());
+          this.newTask = '';
+        }
+      },
+      deleteTask(index) {
+        this.tasks.splice(index, 1);
+      }
+    }
+  };
+  </script>
+  
